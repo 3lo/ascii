33770ec86b5ad4adf8282779feec45dfd98271cb
Eric <41165769+3lo@users.noreply.github.com>
2020-01-04 17:19:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from PIL import Image
+print("Ascii Converter")
+
+os.chdir('C:\\Users\\Eric\\Desktop\\file\\online pictures')
+im = Image.open("spooder.png")
+width, height = im.size
+print(str(width) + " x " + str(height))
+
+pixel_matrix = []
+pixel = im.convert("RGB")
+
+for x in range(width):
+    for y in range(height):
+        pixel_matrix.append([pixel.getpixel((x, y))])
+
+print(len(pixel_matrix))
+
+characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+bright_list = []
+for x in range(len(pixel_matrix)):
+    for y in range(len(pixel_matrix[x])):
+        data = pixel_matrix[x][y]
+        brightness = (data[0] + data[1] + data[2]) / 3  # Convert RGB into Brightness using Average: (R + G + B) / 3
+        brightness = round(brightness)
+        bright_list.append(brightness)  # Convert Brightness into ASCII characters
+
+print(bright_list)
+
+# A brightness of 0 should map to a delicate backtick (`), and a brightness of 255 should map to big stocky dollar sign ($)
+'''
+fmt.Print(string("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"[uint32(brightness)/4]))
+'''
