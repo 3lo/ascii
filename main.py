001c5ae067a8d332f7ab58cd076e50678504e1d6
Eric <41165769+3lo@users.noreply.github.com>
2020-01-05 20:40:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from PIL import Image
 print("Ascii Converter")
 
-os.chdir('C:\\Users\\Eric\\Desktop\\file\\online pictures')
-im = Image.open("spooder.png")
+os.chdir('C:\\Users\\Eric\\Desktop')
+im = Image.open("pineapple.jpg")
 width, height = im.size
 print(str(width) + " x " + str(height))
 
@@ -14,9 +14,8 @@ for x in range(width):
     for y in range(height):
         pixel_matrix.append([pixel.getpixel((x, y))])
 
-print(len(pixel_matrix))
-
-characters = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+# convert into brightness
 bright_list = []
 for x in range(len(pixel_matrix)):
     for y in range(len(pixel_matrix[x])):
@@ -25,7 +24,17 @@ for x in range(len(pixel_matrix)):
         brightness = round(brightness)
         bright_list.append(brightness)  # Convert Brightness into ASCII characters
 
-print(bright_list)
+
+def split(word):
+    return [char for char in word]
+
+
+ascii_list = split(ascii_chars)
+list_word = []
+for i in range(len(bright_list)):
+    num = round(bright_list[i] / 4)
+    list_word.append(ascii_list[num])
+print(''.join(list_word))  # split new lines at the width of the image
 
 # A brightness of 0 should map to a delicate backtick (`), and a brightness of 255 should map to big stocky dollar sign ($)
 '''
